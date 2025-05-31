77ee07d5c9e0b9b8a45f1649fad7a994c6ba012e
onyiafranklin <onyiafranklin89@gmail.com>
2025-05-31 17:54:20 +0100

    update

--- target/classes/db/mysql/user.sql
+++ target/classes/db/mysql/user.sql
@@ -1,7 +1,7 @@
-CREATE DATABASE IF NOT EXISTS petclinic;
+CREATE DATABASE IF NOT EXISTS petadoption;
 
-ALTER DATABASE petclinic
+ALTER DATABASE petadoption
   DEFAULT CHARACTER SET utf8
   DEFAULT COLLATE utf8_general_ci;
 
-GRANT ALL PRIVILEGES ON petclinic.* TO 'petclinic'@'%' IDENTIFIED BY 'petclinic';
+GRANT ALL PRIVILEGES ON petadoption.* TO 'petclinic'@'%' IDENTIFIED BY 'petclinic';
